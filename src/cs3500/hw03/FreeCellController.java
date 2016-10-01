b4af2ce562e7caed54183b8d47055d9c4e17ef80
GentleFan <zhangyf1221@gmail.com>
2016-09-30 21:11:34 -0400

    TODO: run()

--- src/cs3500/hw03/FreeCellController.java
+++ src/cs3500/hw03/FreeCellController.java
@@ -2,6 +2,7 @@ package cs3500.hw03;
 import java.util.List;
 
 import cs3500.hw02.Card;
+import cs3500.hw02.FreeCellModel;
 import cs3500.hw02.IFreeCellModel;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,6 +17,7 @@ public class FreeCellController implements IFreeCellController<Card> {
   // Define fields
   private Readable rd;
   private Appendable ap;
+  private IFreeCellModel<Card> model;
 
 
   /**
@@ -29,6 +31,26 @@ public class FreeCellController implements IFreeCellController<Card> {
 
   public void playGame(List<Card> deck, IFreeCellModel<Card> model, int numCascades,
                        int numOpens, boolean shuffle) {
-
+    this.model = model;
+    model.startGame(deck, numCascades, numOpens, shuffle);
+    try {
+      ap.append(model.getGameState());
+    }
+    catch (IOException e0) {}
+    run();
   }
+
+  private void run() {
+    Scanner s1 = new Scanner(rd);
+    while(!model.isGameOver()) {
+      try {
+        ap.append("\n");
+        ap.append("Enter your move: " +
+                "(SourceType, PileIndex, CardIndex, DestType, PileIndex)");
+        String ST = s1.next();
+      }
+
+    }
+  }
+
 }
